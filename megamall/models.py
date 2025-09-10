1e3e4fc57ca7a0ed9873fad102881dc19902d05f
JeffRebiro <jeffrebiro@gmail.com>
2025-09-10 12:49:38 +0300

    TURKS

--- megamall/models.py
+++ megamall/models.py
@@ -69,6 +69,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     
     image_url = models.URLField(blank=True, null=True)
     def __str__(self):
