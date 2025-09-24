25888534ef9663c39a0651f1ba31ed423ce688ea
JeffRebiro <jeffrebiro@gmail.com>
2025-09-24 20:15:27 +0300

    ADMIN

--- backend/settings.py
+++ backend/settings.py
@@ -26,8 +26,11 @@ else:
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://masterpiece-frontend.vercel.app",
     "https://e-commerce-backend-7yft.onrender.com",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 INSTALLED_APPS = [
     'cloudinary',
@@ -63,8 +66,8 @@ AUTH_USER_MODEL = "megamall.GuestUser"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "corsheaders.middleware.CorsMiddleware",          # <— move this up here
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
