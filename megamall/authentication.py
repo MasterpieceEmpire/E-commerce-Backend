488db79958f6af54ffeed4ecf53b9ed7b0529855
JeffRebiro <jeffrebiro@gmail.com>
2025-09-25 11:07:26 +0300

    ADMIN

--- megamall/authentication.py
+++ megamall/authentication.py
@@ -7,11 +7,9 @@ User = get_user_model()
 class MongoJWTAuthentication(JWTAuthentication):
     def get_user(self, validated_token):
         user_id = validated_token.get("user_id")
-
         if not user_id:
             raise AuthenticationFailed("Token contained no identifiable user", code="no_user_id")
-
         try:
-            return User.objects.get(id=str(user_id))
+            return User.objects.get(id=str(user_id))  # always string
         except User.DoesNotExist:
             raise AuthenticationFailed("User not found", code="user_not_found")
